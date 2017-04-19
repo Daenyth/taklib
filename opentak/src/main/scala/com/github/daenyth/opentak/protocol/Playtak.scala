8953ced098eb03f4d6f9edb23405516e21bcd179
Gavin Bisesi <daenyth@gmail.com>
2017-04-19 17:55:23 -0400

    Add value classes for playtak domain objects

--- opentak/src/main/scala/com/github/daenyth/opentak/protocol/Playtak.scala
+++ opentak/src/main/scala/com/github/daenyth/opentak/protocol/Playtak.scala
@@ -4,46 +4,50 @@ import com.github.daenyth.taklib.{PlayStone, Player, Move => TLMove}
 
 // See protocol description at https://github.com/chaitu236/TakServer
 object Playtak {
+  case class GameNumber(value: Int) extends AnyVal
+  case class Username(value: String) extends AnyVal
+  case class RoomName(value: String) extends AnyVal
+
   sealed trait Incoming
 
   object Incoming {
 
-    case class Client(name: String) extends Incoming
-    case class Register(username: String, email: String) extends Incoming
+    case class Client(version: String) extends Incoming
+    case class Register(username: Username, email: String) extends Incoming
 
     trait Login extends Incoming
-    case class UserLogin(username: String, password: String) extends Login
+    case class UserLogin(username: Username, password: String) extends Login
     case object GuestLogin extends Login
     case object Logout
 
     case class Seek(size: Int, time: Int, increment: Int, asPlayer: Option[Player])
         extends Incoming
-    case class Accept(gameNumber: Int) extends Incoming
+    case class Accept(gameNumber: GameNumber) extends Incoming
 
     /** Commands available to players in a game */
     sealed trait GameCommand extends Incoming {
-      def gameNumber: Int
+      def gameNumber: GameNumber
     }
-    case class Place(gameNumber: Int, playStone: PlayStone) extends GameCommand
-    case class Move(gameNumber: Int, move: TLMove) extends GameCommand
-    case class OfferDraw(gameNumber: Int) extends GameCommand
-    case class RescindDrawOffer(gameNumber: Int) extends GameCommand
-    case class Resign(gameNumber: Int) extends GameCommand
-    case class Show(gameNumber: Int) extends GameCommand
-    case class RequestUndo(gameNumber: Int) extends GameCommand
-    case class RescindUndoRequest(gameNumber: Int) extends GameCommand
+    case class Place(gameNumber: GameNumber, playStone: PlayStone) extends GameCommand
+    case class Move(gameNumber: GameNumber, move: TLMove) extends GameCommand
+    case class OfferDraw(gameNumber: GameNumber) extends GameCommand
+    case class RescindDrawOffer(gameNumber: GameNumber) extends GameCommand
+    case class Resign(gameNumber: GameNumber) extends GameCommand
+    case class Show(gameNumber: GameNumber) extends GameCommand
+    case class RequestUndo(gameNumber: GameNumber) extends GameCommand
+    case class RescindUndoRequest(gameNumber: GameNumber) extends GameCommand
 
     case object ListSeeks extends Incoming
     case object ListGames extends Incoming
 
-    case class Subscribe(gameNumber: Int) extends Incoming
-    case class Unsubscribe(gameNumber: Int) extends Incoming
+    case class Subscribe(gameNumber: GameNumber) extends Incoming
+    case class Unsubscribe(gameNumber: GameNumber) extends Incoming
 
     case class Shout(msg: String) extends Incoming
-    case class JoinRoom(roomName: String) extends Incoming
-    case class ShoutRoom(roomName: String, msg: String) extends Incoming
-    case class LeaveRoom(roomName: String) extends Incoming
-    case class Tell(username: String, msg: String) extends Incoming
+    case class JoinRoom(name: RoomName) extends Incoming
+    case class ShoutRoom(name: RoomName, msg: String) extends Incoming
+    case class LeaveRoom(name: RoomName) extends Incoming
+    case class Tell(username: Username, msg: String) extends Incoming
 
     case object Ping
   }
@@ -53,67 +57,68 @@ object Playtak {
   object Outgoing {
     case object Welcome extends Outgoing
     case object LoginOrRegisterNow extends Outgoing
-    case class WelcomeUser(username: String) extends Outgoing
-    case class GameListAdd(gameNumber: Int,
-                           whitePlayerUsername: String,
-                           blackPlayerUsername: String,
+    case class WelcomeUser(username: Username) extends Outgoing
+    case class GameListAdd(gameNumber: GameNumber,
+                           whitePlayerusername: Username,
+                           blackPlayerusername: Username,
                            size: Int,
                            time: Int,
                            increment: Int,
                            halfMovesPlayed: Int,
                            playerToMove: Player)
         extends Outgoing
-    case class GameListRemove(gameNumber: Int,
-                              whitePlayerUsername: String,
-                              blackPlayerUsername: String,
+    case class GameListRemove(gameNumber: GameNumber,
+                              whitePlayerusername: Username,
+                              blackPlayerusername: Username,
                               size: Int,
                               time: Int,
                               increment: Int,
                               halfMovesPlayed: Int,
                               playerToMove: Player)
         extends Outgoing
-    case class GameStart(gameNumber: Int,
+    case class GameStart(gameNumber: GameNumber,
                          size: Int,
-                         whitePlayerUsername: String,
-                         blackPlayerUsername: String,
+                         whitePlayerusername: Username,
+                         blackPlayerusername: Username,
                          yourColor: Player)
         extends Outgoing
-    case class Place(gameNumber: Int, playStone: PlayStone) extends Outgoing
-    case class Move(gameNumber: Int, move: TLMove) extends Outgoing
-    case class UpdateTime(gameNumber: Int, whiteTime: String, blackTime: String) extends Outgoing // TODO check what type the time values are
+    case class Place(gameNumber: GameNumber, playStone: PlayStone) extends Outgoing
+    case class Move(gameNumber: GameNumber, move: TLMove) extends Outgoing
+    case class UpdateTime(gameNumber: GameNumber, whiteTime: String, blackTime: String)
+        extends Outgoing // TODO check what type the time values are
     case class GameOver(result: String) extends Outgoing
-    case class DrawOffered(gameNumber: Int) extends Outgoing
-    case class DrawOfferRescinded(gameNumber: Int) extends Outgoing
-    case class UndoRequested(gameNumber: Int) extends Outgoing
-    case class UndoRequestRescinded(gameNumber: Int) extends Outgoing
-    case class PerformUndo(gameNumber: Int) extends Outgoing
-    case class GameAbandoned(gameNumber: Int) extends Outgoing
-    case class SeekAdded(gameNumber: Int,
-                         username: String,
+    case class DrawOffered(gameNumber: GameNumber) extends Outgoing
+    case class DrawOfferRescinded(gameNumber: GameNumber) extends Outgoing
+    case class UndoRequested(gameNumber: GameNumber) extends Outgoing
+    case class UndoRequestRescinded(gameNumber: GameNumber) extends Outgoing
+    case class PerformUndo(gameNumber: GameNumber) extends Outgoing
+    case class GameAbandoned(gameNumber: GameNumber) extends Outgoing
+    case class SeekAdded(gameNumber: GameNumber,
+                         username: Username,
                          size: Int,
                          time: String,
                          asPlayer: Option[Player])
         extends Outgoing
-    case class SeekRemoved(gameNumber: Int,
-                           username: String,
+    case class SeekRemoved(gameNumber: GameNumber,
+                           username: Username,
                            size: Int,
                            time: String,
                            asPlayer: Option[Player])
         extends Outgoing
-    case class ObserveGame(gameNumber: Int,
-                           whitePlayerUsername: String,
-                           blackPlayerUsername: String,
+    case class ObserveGame(gameNumber: GameNumber,
+                           whitePlayerusername: Username,
+                           blackPlayerusername: Username,
                            size: Int,
                            time: String,
                            halfMovesPlayed: Int,
                            playerToMove: Player)
         extends Outgoing
-    case class Shout(username: String, msg: String) extends Outgoing
-    case class RoomJoined(name: String) extends Outgoing
-    case class RoomLeft(name: String) extends Outgoing
-    case class ShoutRoom(name: String, username: String, msg: String) extends Outgoing
-    case class Tell(username: String, msg: String) extends Outgoing
-    case class Told(username: String, msg: String) extends Outgoing
+    case class Shout(username: Username, msg: String) extends Outgoing
+    case class RoomJoined(name: RoomName) extends Outgoing
+    case class RoomLeft(name: RoomName) extends Outgoing
+    case class ShoutRoom(name: RoomName, username: Username, msg: String) extends Outgoing
+    case class Tell(username: Username, msg: String) extends Outgoing
+    case class Told(username: Username, msg: String) extends Outgoing
     case class ServerMessage(msg: String) extends Outgoing
     case class Error(msg: String) extends Outgoing
     case class OnlineUsers(count: Int) extends Outgoing
